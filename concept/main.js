ee3b739edcbd0a368dfcb12cd08ef8a1359ad078
Linda Wang <linda@Lindas-MacBook-Air.local>
2016-04-23 13:20:09 -0400

    twokey and skeleton for remaining keys in main

--- concept/main.js
+++ concept/main.js
@@ -1,23 +1,53 @@
 $(function() {
   var dictionary = ["apple", "bird", "cat", "dog", "eagle"]
-  var redirects = {
-    "cat" : "./secret.html",
-  }
+
+  var twokey = false;
+  var password = false;
+
+  var attempts = 0;
+  var limited = false;
+  var exponential = false;
+  var redirects = {};
 
   var init = function() {
+    var current = window.location.href;
+    var start = current.lastIndexOf("/");
+    var end = current.lastIndexOf(".");
+    var page = current.substring(start+1, end);
+
+    if (page === "shoutkey") {
+      redirects = {"cat" : "./secret.html"};
+    } else if (page === "twokey") {
+      twokey = true;
+      redirects = {"birddog" : "./secret.html"};
+    } else if (page === "password" || page === "limited" || page === "exponential") {
+      limited = page === "limited";
+      exponential = page === "exponential";
+      redirects = {"apple" : {"password": dog, "redirect": "./secret.html"}};
+    } else {
+      console.log("unknown page " + page);
+    }
+
+    displayDict();
+  }
+
+  var displayDict = function() {
     dictionary.forEach(function(item) {
       $("#dict").append("<div class='item'>"+item+"</div>");
     });
-
   }
 
   $("#attempt").click(function() {
     var input = document.getElementById("key").value;
+    if (twokey) {
+      input += document.getElementById("key2").value;
+    }
     var redirect = redirects[input];
     if (redirect !== undefined) {
       window.location.replace(redirect);
+    } else {
+      $("#sorry").show();
     }
-    $("#sorry").show();
   });
 
   $(document).on("click", "#input_key", function(){
